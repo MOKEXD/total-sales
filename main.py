46cecada858e1b5f8d43fb93fea9c0d19d83a7e1
RONALDO RODRIGUEZ DE LIMA <165290429+MOKEXD@users.noreply.github.com>
2026-03-11 07:01:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from sisepuede import init
 from total import calculate_total_sales
+from datetime import datetime
+now = datetime.now()
+date = now.strftime("%Y-%m-%d")
+time = now.strftime("%H:%M:%S")
 print("===========================================")
 print("        hello welcome to moke app          ")
 print("===========================================")
+print("Date:", date)
+print("Time:", time)
 init()
 calculate_total_sales()
